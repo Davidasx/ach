3fe07bba9027eb4bcf372269c047efddec534573
Davidasx <81460770+Davidasx@users.noreply.github.com>
2021-03-27 18:28:28 +0800

    Add files via upload

---
+++ install.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	system("choco install python");
+	system("choco install nodejs");
+	system("choco install gh");
+	system("pip3 install online-judge-tools");
+	system("npm install atcoder-cli");
+	return 0;
+}
+
